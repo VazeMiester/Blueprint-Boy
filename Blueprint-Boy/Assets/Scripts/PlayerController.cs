ee469a740d8cb15fa58d3bdf7c7fb4e18abc7973
VazeMiester <149853708+VazeMiester@users.noreply.github.com>
2024-09-14 02:00:48 -0400

    Added scale function

--- Blueprint-Boy/Assets/Scripts/PlayerController.cs
+++ Blueprint-Boy/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -9,15 +10,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;          // Horizontal movement speed
-    public float jumpForce = 20.0f;     // Jump force
-    public float rotationSpeed = 180f;  // Rotation speed
-    private Rigidbody2D rb2D;           // Reference to the Rigidbody2D component
-    private bool isGrounded = true;     // Check if the player is grounded
-    public Transform spawnPoint;        // Reference spawn location
-    private Quaternion targetRotation;   // The target roation for the player
-    private Quaternion rotationToAdd;   // The rotation that needs to be added to the target angle when the player rotates
-    public string gravityDirection = "down";
+    public float speed = 5.0f;               // Horizontal movement speed
+    public float jumpForce = 20.0f;          // Jump force
+    public float rotationSpeed = 1000f;      // Rotation speed
+    private Rigidbody2D rb2D;                // Reference to the Rigidbody2D component
+    private bool isGrounded = true;          // Check if the player is grounded
+    public Transform spawnPoint;             // Reference spawn location
+    private Quaternion targetRotation;       // The target roation for the player
+    private Quaternion rotationToAdd;        // The rotation that needs to be added to the target angle when the player rotates
+    public string gravityDirection = "down"; // The current direction of gravity
+    public float speedLimit = 5f;            // The velocity of the player where force stops being added when the movement key is pressed
+    public float currentScale = 1;           // A value representing the current scale of the game
+    public CinemachineVirtualCamera virtualCamera;
 
     void Start()
     {
@@ -32,11 +36,11 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxisRaw("Horizontal");         //get the button input for horizontal movement
         Vector2 localRight = transform.right * moveHorizontal * speed; //normalize the vector for local orientation and muliply by speed
 
-
+        
         // move using velocity
         //if (gravityDirection == "down" || gravityDirection == "up")    //move horizontally based on the players orientation
         //{
-        //    rb2D.velocity = new Vector2(localRight.x, rb2D.velocity.y); 
+        //    rb2D.velocity = new Vector2(localRight.x, rb2D.velocity.y);
         //}
         //else if (gravityDirection == "right" || gravityDirection == "left")
         //{
@@ -44,7 +48,13 @@ public class PlayerController : MonoBehaviour
         //}
 
         //move using force
-        rb2D.AddForce(localRight);
+        Vector2 localVelocity = transform.InverseTransformDirection(rb2D.velocity);
+        float horizontalLocalVelocity = localVelocity.x;
+
+        if ((horizontalLocalVelocity <= speedLimit && moveHorizontal == 1) || (horizontalLocalVelocity >= -speedLimit && moveHorizontal == -1))  // only add force if the player is going less than the speedlimit
+        {
+            rb2D.AddForce(localRight);
+        }
 
         // control jumping
         Vector2 localUp = transform.up;
@@ -55,6 +65,8 @@ public class PlayerController : MonoBehaviour
             isGrounded = false; // Set grounded to false after jumping
         }
 
+
+        // control map rotation
         if (Input.GetKeyDown(KeyCode.Z)) // Check for Z key press
         {
             RotateLeft();
@@ -68,6 +80,17 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); // always rotate towards the target rotation
 
         UpdateGravityBasedOnRotation();
+
+        // control map scale
+        if (Input.GetKeyDown (KeyCode.Q) && currentScale != 1)
+        {
+            ScaleDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && currentScale != 7)
+        {
+            ScaleUp();
+        }
     }
 
     void UpdateGravityBasedOnRotation()
@@ -120,5 +143,19 @@ public class PlayerController : MonoBehaviour
     {
         rotationToAdd = Quaternion.Euler(0, 0, 90);
         targetRotation = targetRotation * rotationToAdd;
+    }
+
+    public void ScaleDown()
+    {
+        transform.localScale = new Vector2(transform.localScale.x / 2, transform.localScale.y / 2);
+        currentScale = currentScale - 1;
+        virtualCamera.m_Lens.OrthographicSize = virtualCamera.m_Lens.OrthographicSize / 2;
+    }
+
+    public void ScaleUp()
+    {
+        transform.localScale = new Vector2(transform.localScale.x * 2, transform.localScale.y * 2);
+        currentScale = currentScale + 1;
+        virtualCamera.m_Lens.OrthographicSize = virtualCamera.m_Lens.OrthographicSize * 2;
     }
 }
